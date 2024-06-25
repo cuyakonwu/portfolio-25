f3ba224585a4106cc1b4e63e8477fc04bff5633b
cuyakonwu <cuyakonwu@uchicago.edu>
2024-06-25 18:54:02 -0400

    Added and updated experiences, completed Jinja2 template

--- app/__init__.py
+++ app/__init__.py
@@ -12,8 +12,49 @@ def index():
 
 @app.route('/experience')
 def experience():
-    return render_template('experience.html')
+    title = "Conrad Uyakonwu's Portfolio"
+    work_experiences = [
+        {
+            "title": "System Administrator and Technical Support",
+            "company": "Computer Science Instructional Lab (CSIL)",
+
+            "description": "As part of my role, I conducted a mini-course on AI \
+             and data science, educating patrons on various methodologies. I \
+             spearheaded a CSIL-wide wiki audit, ensuring the accuracy and \
+             relevance of documentation, which resulted in streamlined access \
+             to critical information for all users. Additionally, I coordinated \
+             with the education instruction team to organize and facilitate \
+             inter-departmental courses, significantly enhancing the overall \
+             technical proficiency of CSIL staff.",
+
+            "duration": "March 2024 - Present"
+        },
+        {
+            "title": "WIP",
+            "company": "Major League Hacking",
+            "description": "WIP",
+            "duration": "June 2024 - September 2024"
+        },
+        {
+
+        }
+    ]
+
+    education = [
+        {
+            "degree": "Bachelor's in Computer Science",
+            "institution": "University of Chicago",
+            "description": "Specialized in Machine Learning",
+            "year": "Expected 2026"
+        }
+    ]
+    return render_template('experience.html', title=title, work_experiences=work_experiences, education=education,)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    hobbies = [
+        "Playing the piano",
+        "Hiking",
+        "Reading science fiction novels"
+    ]
+    return render_template('about.html', title=title, hobbies=hobbies)
