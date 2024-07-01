5f7c9b560f200710b8e9c7524f2fe5245e7a5021
cuyakonwu <cuyakonwu@uchicago.edu>
2024-07-01 18:31:45 -0400

    Setting port and host

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
+
+app = Flask(__name__)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
 
