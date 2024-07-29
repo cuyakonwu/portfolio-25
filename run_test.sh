89e47afe637170e9b3eca80b30d6783b7d3034f4
cuyakonwu <cuyakonwu@uchicago.edu>
2024-07-29 18:06:50 -0400

    Changed tests to reflect not having the site on port 5000

--- run_test.sh
+++ run_test.sh
@@ -2,8 +2,8 @@
 
 $PWD/python3-virtualenv/bin/python -m unittest discover -v tests/
 
-curl --request GET http://127.0.0.1:5000//api/timeline_post
+curl --request GET http://127.0.0.1//api/timeline_post
 
-curl --request POST http://127.0.0.1:5000//api/timeline_post -d 'name=Conrad&email=cuyakonwu@mlh.io&content=Just Added Database to my portfolio site!'
+curl --request POST http://127.0.0.1//api/timeline_post -d 'name=Conrad&email=cuyakonwu@mlh.io&content=Just Added Database to my portfolio site!'
 
-curl --request DELETE http://127.0.0.1:5000/api/timeline_post/1
+curl --request DELETE http://127.0.0.1/api/timeline_post/1
